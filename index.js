894b0c9d623ae5124ada1ab810917f9bbd4d7c9b
Rachel Rondon <rachel.sun.rondon@gmail.com>
2019-08-18 15:20:17 -0400

    CodeWars: Even or Odd

--- index.js
+++ index.js
@@ -30,3 +30,13 @@ likes(["Peter"]);
 likes(["Jacob, Alex"]);
 likes(["Max", "John", "Mark"]);
 likes(["Alex", "Jacob", "Mark", "Max"]);
+
+
+/* Even or Odd */
+/* Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers */
+
+function evenOdd(number) {
+
+}
+
+evenOdd(5);
